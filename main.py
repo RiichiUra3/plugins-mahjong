dbfff54ba3cccf6a1163f57b5459595bbfdf291f
Yansen Wang <yansenwang@microsoft.com>
2023-06-09 17:21:31 +0800

    add paili to plugin and finished the pipeline

--- main.py
+++ main.py
@@ -4,31 +4,15 @@ import quart
 import quart_cors
 from quart import request
 
+from utils import get_paili_json
+
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
-# Keep track of todo's. Does not persist if Python session is restarted.
-_TODOS = {}
 
-@app.post("/todos/<string:username>")
-async def add_todo(username):
-    request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
-    return quart.Response(response='OK', status=200)
-
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
-
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
-    request = await quart.request.get_json(force=True)
-    todo_idx = request["todo_idx"]
-    # fail silently, it's a simple plugin
-    if 0 <= todo_idx < len(_TODOS[username]):
-        _TODOS[username].pop(todo_idx)
-    return quart.Response(response='OK', status=200)
+@app.get("/mahjong_paili/<string:hand>")
+async def get_paili(hand):
+    print(hand)
+    return quart.Response(response=get_paili_json(hand), status=200, mimetype="text/json")
 
 @app.get("/logo.png")
 async def plugin_logo():
--- utils.py
+++ utils.py
@@ -1,3 +1,20 @@
+import json
+
 from mahjong_utils.models.tile import parse_tiles
 from mahjong_utils.shanten import shanten
 
+def get_paili_json(hand):
+    print(hand)
+    result = shanten(parse_tiles(hand))
+    result_array = []
+    for tile in result.discard_to_advance:
+        result_discard_tile = result.discard_to_advance[tile]
+        result_array.append({
+            "discard": str(tile),
+            "shanten": result_discard_tile.shanten,
+            "advance": [] if result_discard_tile.advance is None else [str(t) for t in result_discard_tile.advance],
+            "advance_num": result_discard_tile.advance_num or 0,
+            "good_shape_advance": [] if result_discard_tile.good_shape_advance is None else [str(t) for t in result_discard_tile.good_shape_advance],
+            "good_shape_advance_num": result_discard_tile.good_shape_advance_num or 0
+        })
+    return json.dumps(result_array)
